6cb55cc2d84cedb7816d4cc4ed6d2fcaa79ab962
infinitetrooper <akhil.nallaparaju@gmail.com>
2023-12-27 12:34:32 +0530

    Initial Commit

---
+++ heimdall/heimdallApp.swift
@@ -0,0 +1,17 @@
+//
+//  heimdallApp.swift
+//  heimdall
+//
+//  Created by Akhil Varma on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct heimdallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
