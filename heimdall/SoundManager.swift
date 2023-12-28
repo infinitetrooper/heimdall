bf14b0a2fca4782452c66fe512f92a695df6aefb
infinitetrooper <akhil.nallaparaju@gmail.com>
2023-12-28 20:40:56 +0530

    buffing...move to audiokit

--- heimdall/SoundManager.swift
+++ heimdall/SoundManager.swift
@@ -1,71 +1,35 @@
-import AVFoundation
+import AudioKit
+import SoundpipeAudioKit
 
 class SoundManager {
-    var audioEngine: AVAudioEngine
-    var playerNode: AVAudioPlayerNode
-    var buffer: AVAudioPCMBuffer
+    var engine: AudioEngine
+    var oscillator: Oscillator
 
     init() {
-        audioEngine = AVAudioEngine()
-        playerNode = AVAudioPlayerNode()
+        engine = AudioEngine()
+        oscillator = Oscillator()
+        oscillator.amplitude = 0.5  // Set the amplitude
 
-        let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 1)
-        let frameCapacity = AVAudioFrameCount(format?.sampleRate ?? 44100)
-        buffer = AVAudioPCMBuffer(pcmFormat: format!, frameCapacity: frameCapacity)!
+        engine.output = oscillator
 
-        fillBuffer(withFrequency: 440) // A default frequency
-
-        audioEngine.attach(playerNode)
-        audioEngine.connect(playerNode, to: audioEngine.outputNode, format: format)
-
-        try? audioEngine.start()
-        playerNode.play()
-    }
-
-    func fillBuffer(withFrequency frequency: Float) {
-        let sampleRate = buffer.format.sampleRate
-        let length = sampleRate * 1.0 // 1 second buffer
-        buffer.frameLength = AVAudioFrameCount(length)
-
-        let waveLength = sampleRate / Double(frequency)
-        let amplitude: Float = 0.5
-
-        for frame in 0..<Int(buffer.frameLength) {
-            let angle = (2.0 * .pi / waveLength) * Double(frame)
-            let sample = Float(sin(angle)) * amplitude
-            buffer.floatChannelData?.pointee[frame] = sample
+        do {
+            try engine.start()
+        } catch {
+            print("Error starting AudioKit engine: \(error)")
         }
     }
 
     func start() {
-        if !audioEngine.isRunning {
-            do {
-                try audioEngine.start()
-                playerNode.play()
-                print("Sound started")
-            } catch {
-                print("Error starting sound: \(error)")
-            }
-        }
+        oscillator.start()
+        print("Sound started")
     }
 
     func stop() {
-        if audioEngine.isRunning {
-            playerNode.stop()
-            audioEngine.stop()
-            print("Sound stopped")
-        }
+        oscillator.stop()
+        print("Sound stopped")
     }
 
-    func updateFrequency(frequency: Float) {
-        fillBuffer(withFrequency: frequency)
-        playerNode.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
-    }
-    
-
-
-    deinit {
-        playerNode.stop()
-        audioEngine.stop()
+    func updateFrequency(frequency: AUValue) {
+        oscillator.frequency = frequency
     }
 }
