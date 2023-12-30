67b4cdaa0471282908f80c1dc2413dcf339224e4
infinitetrooper <akhil.nallaparaju@gmail.com>
2023-12-30 21:39:52 +0530

    added colour overlay while playing note

--- heimdall/ContentView.swift
+++ heimdall/ContentView.swift
@@ -13,67 +13,80 @@ struct ContentView: View {
     private var notePlayer = NotePlayer()
     @State private var noteQueue: [Double] = []
     @State private var currentNoteIndex = 0
+    @State private var currentColor = UIColor.clear
 
     var body: some View {
-        VStack {
-            ZStack {
-                CameraPreviewView(cameraManager: cameraManager)
-
-                // ISO Display at the bottom
-                VStack {
-                    Spacer()
-                    HStack {
-                        Text("ISO: \(Int(cameraManager.currentISO))")
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.black.opacity(0.7))
-                            .cornerRadius(10)
-
+        GeometryReader { geometry in
+            
+            VStack {
+                ZStack {
+                    CameraPreviewView(cameraManager: cameraManager)
+                    
+                    // ISO Display at the bottom
+                    VStack {
                         Spacer()
-
-                        // Toggle for Sound Playing
-                        Toggle(isOn: $isSoundPlaying) {
-                            Text(isSoundPlaying ? "Stop Sound" : "Start Sound")
-                        }
-                        .padding()
-                        .onChange(of: isSoundPlaying) { newValue in
-                            if newValue {
-                                cameraManager.startSound()
-                            } else {
-                                cameraManager.stopSound()
+                        HStack {
+                            Text("ISO: \(Int(cameraManager.currentISO))")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .cornerRadius(10)
+                            
+                            Spacer()
+                            
+                            // Toggle for Sound Playing
+                            Toggle(isOn: $isSoundPlaying) {
+                                Text(isSoundPlaying ? "Stop Sound" : "Start Sound")
+                            }
+                            .padding()
+                            .onChange(of: isSoundPlaying) { newValue in
+                                if newValue {
+                                    cameraManager.startSound()
+                                } else {
+                                    cameraManager.stopSound()
+                                }
                             }
                         }
-                    }
-                    .padding()
-                }
-            }
-            Spacer() // Pushes everything above to the top
-            Button("Capture and Play") {
-                cameraManager.captureImage { image in
-                    if let image = image {
-                        processAndPlayImage(image)
+                        .padding()
                     }
                 }
+                Spacer() // Pushes everything above to the top
+                Button("Capture and Play") {
+                    cameraManager.captureImage { image in
+                        if let image = image {
+                            processAndPlayImage(image)
+                        }
+                    }
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .font(.headline)
+                .cornerRadius(10)
+                .padding(20)
             }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .font(.headline)
-            .cornerRadius(10)
-            .padding(.bottom, 10)
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .overlay(
+                Color(currentColor)
+                    .opacity(0.5) // Adjust opacity for translucency
+                    .edgesIgnoringSafeArea(.all)
+            )
         }
-        
+        .edgesIgnoringSafeArea(.all)
         .onAppear() {
             cameraManager.stopSound()
             isSoundPlaying = false
+            notePlayer.onColorChange = { color in
+                self.currentColor = color
+            }
         }
     }
     
     private func processAndPlayImage(_ image: UIImage) {
-        let segmentedColors = getSegmentedColors(from: image, gridRows: 10, gridColumns: 10)
-        let notes = segmentedColors.map(mapColorToNote)
+        let colors = getSegmentedColors(from: image, gridRows: 10, gridColumns: 10)
+        let notes = colors.map(mapColorToNote)
 
-        notePlayer.playNotesInRhythm(notes, baseNoteDuration: 0.2, gapDuration: 0.1)
+        notePlayer.playNotesInRhythm(notes, colors: colors, baseNoteDuration: 0.2, gapDuration: 0.1)
         //notePlayer.playNotesSequence(notes, noteDuration: 0.25)
     }
 }
--- heimdall/NotePlayer.swift
+++ heimdall/NotePlayer.swift
@@ -1,9 +1,11 @@
 import AudioKit
+import UIKit
 import AVFoundation
 
 class NotePlayer {
     var engine: AudioEngine
     var sampler: AppleSampler
+    var onColorChange: ((UIColor) -> Void)?
 
     init() {
         engine = AudioEngine()
@@ -26,19 +28,20 @@ class NotePlayer {
         }
     }
     
-    func playNotesInRhythm(_ notes: [MIDINoteNumber], baseNoteDuration: TimeInterval, gapDuration: TimeInterval) {
+    func playNotesInRhythm(_ notes: [MIDINoteNumber], colors: [UIColor], baseNoteDuration: TimeInterval, gapDuration: TimeInterval) {
         var cumulativeDelay: TimeInterval = 0
         var lastNote: MIDINoteNumber? = nil
         var currentNoteDuration = baseNoteDuration
 
-        for note in notes {
+        for (index, note) in notes.enumerated() {
+            let color = colors[index]
             if note == lastNote {
                 // If the note is the same as the last one, increase its duration
                 currentNoteDuration += baseNoteDuration
             } else {
                 // If it's a new note, play the last note with the accumulated duration
                 if let lastNote = lastNote {
-                    scheduleNote(lastNote, at: cumulativeDelay, duration: currentNoteDuration)
+                    scheduleNote(lastNote, color: color, at: cumulativeDelay, duration: currentNoteDuration)
                     cumulativeDelay += currentNoteDuration + gapDuration
                 }
                 // Reset the duration and set the new note as the last note
@@ -48,13 +51,14 @@ class NotePlayer {
         }
         // Schedule the last note
         if let lastNote = lastNote {
-            scheduleNote(lastNote, at: cumulativeDelay, duration: currentNoteDuration)
+            scheduleNote(lastNote, color: UIColor.clear, at: cumulativeDelay, duration: currentNoteDuration)
         }
     }
 
-    private func scheduleNote(_ note: MIDINoteNumber, at delay: TimeInterval, duration: TimeInterval) {
+    private func scheduleNote(_ note: MIDINoteNumber, color: UIColor, at delay: TimeInterval, duration: TimeInterval) {
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
             self?.play(note, duration: duration)
+            self?.onColorChange?(color)
         }
     }
 
